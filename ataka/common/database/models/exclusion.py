578d71480aada392203cab0be224c4efe7f62ea3
Andrea Biondo <andrea.biondo965@gmail.com>
2022-06-09 18:18:40 +0200

    common: add Exclusion model

---
+++ ataka/common/database/models/exclusion.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from ..config import Base, JsonBase
+
+
+class Exclusion(Base, JsonBase):
+    __tablename__ = "exclusions"
+
+    exploit_history_id = Column(String, ForeignKey("exploit_histories.id"),
+                                primary_key=True)
+    target_ip = Column(String, primary_key=True)
