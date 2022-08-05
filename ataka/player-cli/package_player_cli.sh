8b3e7d16f2032491fe108ab1ccead37877377409
Christodoulos Sotiriou <schris15@hotmail.com>
2022-08-05 12:11:05 +0300

    bundling dependencies in player-cli

---
+++ ataka/player-cli/package_player_cli.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -u
+
+TMPFILE="$(mktemp -d)"
+trap "rm -rf '$TMPFILE'" 0               # EXIT
+trap "rm -rf '$TMPFILE'; exit 1" 2       # INT
+trap "rm -rf '$TMPFILE'; exit 1" 1 15    # HUP TERM
+
+cd "$TMPFILE"
+cp -r /ataka/player-cli .
+cp "/ataka/ctfconfig/$CTF.py" player-cli/player_cli/ctfconfig.py
+pip install -r player-cli/requirements.txt --target player-cli/
+python -m zipapp --python "/usr/bin/env python3" --output /ataka/player-cli/ataka-player-cli.pyz player-cli/
