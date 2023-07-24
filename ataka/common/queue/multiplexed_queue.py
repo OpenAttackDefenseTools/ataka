2350ef4e40c484d4b76af2cb40603ce22d27b7d5
DaClemens <git@daclemens.eu>
2023-07-24 15:56:39 +0200

    update database and queue library dependencies

--- ataka/common/queue/multiplexed_queue.py
+++ ataka/common/queue/multiplexed_queue.py
@@ -48,62 +48,11 @@ class MultiplexedQueue(Queue):
     async def call_consumers(self, message: IncomingMessage):
         await asyncio.gather(*[callback(message) for callback in self._callbacks.values()])
 
-    @property
-    def channel(self) -> aiormq.Channel:
-        return self._queue.channel
-
-    @property
-    def loop(self):
-        return self._queue.loop
-
-    @property
-    def name(self):
-        return self._queue.name
-
-    @property
-    def durable(self):
-        return self._queue.durable
-
-    @property
-    def exclusive(self):
-        return self._queue.exclusive
-
-    @property
-    def auto_delete(self):
-        return self._queue.auto_delete
-
-    @property
-    def arguments(self):
-        return self._queue.arguments
-
-    @property
-    def passive(self):
-        return self._queue.passive
-
-    @property
-    def declaration_result(self):
-        return self._queue.declaration_result
-
-    async def declare(self, *args, **kwargs):
-        return await self._queue.declare(*args, **kwargs)
-
-    async def bind(self, *args, **kwargs):
-        return await self._queue.bind(*args, **kwargs)
-
-    async def unbind(self, *args, **kwargs):
-        return await self._queue.unbind(*args, **kwargs)
-
-    async def get(self, *args, **kwargs):
-        return await self._queue.get(*args, **kwargs)
-
-    async def purge(self, *args, **kwargs):
-        return await self._queue.purge(*args, **kwargs)
-
-    async def delete(self, *args, **kwargs):
-        return await self._queue.delete(*args, **kwargs)
+    def __getattr__(self, item):
+        return getattr(self._queue, item)
 
     def __aiter__(self, ):
         return self.iterator()
 
     def iterator(self, *args, **kwargs):
-        return QueueIterator(self, **kwargs)
+        return QueueIterator(self, *args, **kwargs)
